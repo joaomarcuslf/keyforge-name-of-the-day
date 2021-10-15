ba495fcaa0df4d1927dbec31983f1515e23eeb44
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2021-10-15 00:45:20 -0300

    Update go mod

--- main.go
+++ main.go
@@ -4,15 +4,9 @@ import (
 	"os"
 
 	"github.com/joaomarcuslf/keyforge-name-of-the-day/server"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
 	server := server.NewServer(os.Getenv("PORT"))
 
 	server.Run()
